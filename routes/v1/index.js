c53fd7e95e57b4c9285169c52b8665a1d341a3d4
jiwon-yang <jiwonyang.tech@gmail.com>
2019-11-14 21:07:44 +0900

    project init - Router 생성

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const userRouter = require("./api/userRouter");
 const teamRouter = require("./api/teamRouter");
 const candidateRouter = require("./api/candidateRouter");
+const authRouter = require("./api/authRouter");
 
 const router = express.Router();
 
-//router.use("/users", userRouter);
-//router.use("/teams", teamRouter);
-//router.use("/candidates", candidateRouter);
+router.use("/auth", authRouter); //지원
+router.use("/users", userRouter); //지영
+router.use("/teams", teamRouter);
+router.use("/candidates", candidateRouter);
 
 module.exports = router;
