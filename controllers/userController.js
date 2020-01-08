8119d11195a514b3d518fbb2ab95897b88ccc0ee
JaeyunKang <kjy13411@gmail.com>
2020-01-09 00:07:15 +0900

    UserController refix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,9 @@
 const UserService = require('../services/UserService')
-const AuthService = require('../services/AuthService')
 
 const getUserInfo = async (req, res) => {
   const userService = new UserService()
-  const authService = new AuthService()
   try {
     const userInfo = await userService.getUserInfo(req.params.id)
-    const address = userInfo.authenticated_address
-    const school = await authService.findSchoolByEmail(address)
-    const schoolName = school.name
-    userInfo.schoolName = schoolName
-    delete userInfo.authenticated_address
     if (userInfo === null) {
       res.status(404).json({ errorMessage: '사용자가 존재하지 않음' })
     } else {
