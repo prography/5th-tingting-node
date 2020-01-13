c1d97f2a0efb99f52d3e7cb34a1267ce90e25595
jjym12 <jiyoung080912@gmail.com>
2020-01-13 20:32:39 +0900

    ismatched 수정

--- models/MatchingModel.js
+++ models/MatchingModel.js
@@ -10,7 +10,7 @@ class MatchingModel {
   async checkIsMatched (teamId) {
     const accepts = await Matching.findAll({
       where: {
-        receive_team_id: teamId,
+        [Op.or]: [{ send_team_id: teamId }, { receive_team_id: teamId }],
         receive_accept_all: 1,
         is_deleted: 0
       }
