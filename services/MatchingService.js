5f497bf32d6663cbb9a0562bee07af325ae69b33
JaeyunKang <kjy13411@gmail.com>
2020-01-30 07:42:14 +0000

    hotfix for team leave

--- services/MatchingService.js
+++ services/MatchingService.js
@@ -54,7 +54,7 @@ class MatchingService {
   async deleteMatchingdata (teamId) {
     try {
       // matching id 찾기 matching deleted 1
-      const matchings = await this.matchingModel.findMatchingIdsByTeamId(teamId)
+      const matchings = await this.matchingModel.findMatchingsByTeamId(teamId)
       await this.matchingModel.deleteMatchingByTeamId(teamId)
       // accept apply matching id로 지우기
       for (const matching of matchings) {
