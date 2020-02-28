17c21b24fdb5df2b12f70ef28d971ec06553a136
jiwon-yang <jiwonyang.tech@gmail.com>
2020-02-28 11:35:45 +0900

    update findIdAndResetPw

---
+++ models/entities/AuthPassoword.entity.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize')
+const db = require('../../loaders/dbLoader')
+
+const AuthPassword = db.define(
+  'auth',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    authenticated_email: {
+      type: Sequelize.STRING(45),
+      allowNull: false
+    },
+    is_authenticated: {
+      type: Sequelize.INTEGER(1)
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: db.literal('CURRENT_TIMESTAMP')
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: db.literal('CURRENT_TIMESTAMP'),
+      onUpdate: db.literal('CURRENT_TIMESTAMP')
+    }
+  },
+  {
+    tableName: 'auth_password',
+    freezeTableName: true,
+    underscored: true,
+    timestamps: false
+  }
+)
+module.exports = AuthPassword
